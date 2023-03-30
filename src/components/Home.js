36cf067f7bcd5704234b8668ec727c7f266c7e20
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-30 17:33:40 +0000

    create book components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
 import AddBook from './Addbook';
+import Book from './Book';
 
-const Home = () => (
-  <AddBook />
-);
+const Home = () => {
+  const data = {
+    title: 'Hello',
+    author: 'Matthieu',
+    chapter: 'Chapitre 4',
+    completed: 14,
+  };
+
+  console.log(data);
+
+  return (
+    <>
+      <Book data={data} />
+      <AddBook />
+    </>
+  );
+};
 
 export default Home;
