4e90dbceb3ec034b4e4377239082494ec7e48c52
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-04 13:19:46 +0000

    Pass store in the provider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Provider>
+      <Provider store={store}>
         <App />
       </Provider>
     </Router>
