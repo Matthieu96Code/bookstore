5e159e59ddb3d0cf4113d37085f8a3a5e96db375
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-04 15:20:06 +0000

    Add use state to get the submited form

--- src/components/Addbook.js
+++ src/components/Addbook.js
@@ -1,14 +1,26 @@
-const AddBook = () => (
-  <div>
-    <h2 className="addbook-title">
-      ADD NEW BOOK
-    </h2>
-    <form className="addbook-fom">
-      <input type="text" placeholder="Book title" required />
-      <input type="text" placeholder="Book author" required />
-      <button type="button">ADD BOOK</button>
-    </form>
-  </div>
-);
+import { useState } from 'react';
+
+const AddBook = () => {
+  const [titleValue, setTitleValue] = useState();
+  const [authorValue, setAuthorValue] = useState();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <h2 className="addbook-title">
+        ADD NEW BOOK
+      </h2>
+      <form onSubmit={onSubmit} className="addbook-fom">
+        <input value={titleValue} onChange={(event) => setTitleValue(event.target.value)} type="text" placeholder="Book title" required />
+        <input value={authorValue} onChange={(event) => setAuthorValue(event.target.value)} type="text" placeholder="Book author" required />
+        <button type="submit">
+          ADD BOOK
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
