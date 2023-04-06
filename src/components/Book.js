08fd396cbfe54b0549253ae81476e1b9e5dd4078
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-06 17:23:35 +0000

    Use dispach and remove on book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,22 +1,26 @@
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import { removeBook } from '../redux/books/booksSlice';
+import { removeBooks, getBooks } from '../redux/books/booksSlice';
 
-const Book = ({ data }) => {
+const Book = ({ data, id }) => {
   const dispatch = useDispatch();
+  const { category, title, author } = data;
+
+  const handleRemoveBook = async () => {
+    await dispatch(removeBooks(id));
+    await dispatch(getBooks());
+  };
   return (
     <section className="book-card">
       <div>
-        <h1>{data.category}</h1>
-        <h2>{data.title}</h2>
-        <h3>{data.author}</h3>
+        <h1>{category}</h1>
+        <h2>{title}</h2>
+        <h3>{author}</h3>
         <div>
           <button type="button">Comments</button>
           <button
             type="button"
-            onClick={() => {
-              dispatch(removeBook(data.item_id));
-            }}
+            onClick={handleRemoveBook}
           >
             Remove
           </button>
@@ -39,8 +43,8 @@ const Book = ({ data }) => {
 };
 
 Book.propTypes = {
+  id: PropTypes.string.isRequired,
   data: PropTypes.shape({
-    item_id: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
