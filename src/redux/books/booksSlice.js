3aec04180a140e8d7346d0b32114a7100c546d77
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-06 17:10:59 +0000

    Get an apps_id from the api

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,6 +1,8 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ChlLvvO52kCUz8lnKZM5/books';
+
 const initialState = {
   books: [],
   isLoading: true,
